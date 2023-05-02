550d9e5604d1a8899ca3ea4e9f2196381983a708
zbigolo2 <132271271+zbigolo2@users.noreply.github.com>
2023-05-02 10:51:38 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded',function (){
-    var login = document.getElementById('logowanie');
-    login.addEventListener('click',function(){
-        alert('Strona w trakcie budowy');
-    })
-})
